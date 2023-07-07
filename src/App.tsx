ace13c8e37b4518b0153515fb079348e4e81634d
mansidhamelia <mansidhamelia6@gmail.com>
2023-07-07 13:10:24 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+
+const Login = () => {
+    return (
+
+        <div
+            className="relative overflow-hidden py-20 sm:py-28 bg-cyan-900"
+            id="about"
+        ></div>
+
+    )
+}
+
+export default Login
